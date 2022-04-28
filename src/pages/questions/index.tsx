b0ca6f22ea6d81c0ca0377fb9556302322e04b8b
glorydream.top <hanwenhui520@gmail.com>
2022-04-28 17:33:38 +0800

    update fileName

--- src/pages/questions/index.tsx
+++ src/pages/questions/index.tsx
@@ -1,88 +1,70 @@
 import { ProgressBar, Button } from "antd-mobile";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./index.less";
-
-enum questionType {
-  single = "single",
-  multiple = "multiple",
-}
+import _questionsJson from "../../questions.json";
 
 interface question {
   id: number;
   content: string;
-  options: option[];
-  mode: questionType;
+  options?: option[];
+  mode: "single" | "multiple";
+  drag: boolean;
 }
 
 interface option {
   id: number;
   content: string;
+  pic: string;
   belong: number;
+  val: number;
 }
 
+const questionsJson = JSON.parse(JSON.stringify(_questionsJson));
+
+const makeQA = (): question[] => {
+  const questions: question[] = questionsJson.questions;
+  const options: option[] = questionsJson.options;
+  options.forEach((option) => {
+    const ques = questions.find((q) => q.id === option.belong)!;
+    console.log(option.id);
+
+    if (ques.options) {
+      ques.options?.push(option);
+    } else {
+      ques.options = [option];
+    }
+  });
+  console.log(questions);
+
+  return questions;
+};
+
+const q = makeQA();
+
 export default () => {
-  const [qusetions, setQuestions] = useState<question[]>([
-    {
-      id: 1,
-      content:
-        "你是否喜欢吃苹果？你是否喜欢吃苹果？你是否喜欢吃苹果？你是否喜欢吃苹果？你是否喜欢吃苹果？",
-      options: [
-        {
-          id: 1,
-          content: "是",
-          belong: 1,
-        },
-        {
-          id: 2,
-          content: "否",
-          belong: 1,
-        },
-      ],
-      mode: questionType.single,
-    },
-    {
-      id: 2,
-      content: "早餐想吃什么",
-      options: [
-        {
-          id: 3,
-          content: "苹果/牛奶",
-          belong: 2,
-        },
-        {
-          id: 4,
-          content: "面包/咖啡",
-          belong: 2,
-        },
-        {
-          id: 5,
-          content: "豆腐脑/油条",
-          belong: 2,
-        },
-        {
-          id: 6,
-          content: "牛肉拉面",
-          belong: 2,
-        },
-      ],
-      mode: questionType.single,
-    },
-  ]);
+  // 题目列表
+  const [qusetions] = useState<question[]>(q);
+  // 答案集
+  const [answers] = useState<{ [key: number]: number[] }>({});
+  // 进度条
   const [progress, setProgress] = useState<number>(0);
-  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
-  // 去掉勾选状态，更新进度
-  useEffect(() => {
-    setProgress((currentQuestion / qusetions.length) * 100);
-    setSelected(null);
-  },[currentQuestion]);
+  // 当前第几个问题
+  const [questionIndex, setCurrentQuestion] = useState<number>(0);
+  // 选中的答案
   const [selected, setSelected] = useState<number | null>(1);
   // 下一题
   const next = () => {
-    setCurrentQuestion(currentQuestion + 1);
+    setCurrentQuestion(questionIndex + 1);
+    // 去掉勾选状态，更新进度
+    setProgress(((questionIndex + 1) / qusetions.length) * 100);
+    setSelected(null);
   };
   // 上一题
   const pre = () => {
-    setCurrentQuestion(currentQuestion - 1);
+    setCurrentQuestion(questionIndex - 1);
+    // 去掉勾选状态，更新进度
+    setProgress((questionIndex / qusetions.length) * 100);
+    setSelected(null);
   };
   return (
     <>
@@ -94,17 +76,17 @@ export default () => {
             borderRadius: "0",
           }}
         ></ProgressBar>
-        <span className="float-right">{`${currentQuestion}/${
-          qusetions.length
-        }`}</span>
+        <span className="float-right">{`${questionIndex}/${qusetions.length}`}</span>
       </div>
       <div className="p-4 mt-8">
         <div className="mb-12 leading-8">
-          <span className="text-3xl text-green-500 font-bold">{`Q${currentQuestion+1}`}</span>
-          <span>{qusetions[currentQuestion].content}</span>
+          <span className="text-3xl text-green-500 font-bold">{`Q${
+            questionIndex + 1
+          }`}</span>
+          <span>{qusetions[questionIndex].content}</span>
         </div>
-        <div className="px-4 flex flex-col gap-6 min-h-90">
-          {qusetions[currentQuestion].options.map((option, index) => {
+        <div className="px-4 flex flex-col gap-6 min-h-90 pb-20">
+          {qusetions[questionIndex].options!.map((option, index) => {
             return (
               <div
                 onClick={() => setSelected(option.id)}
@@ -115,13 +97,19 @@ export default () => {
                 key={option.id}
               >
                 <span>{String.fromCharCode(65 + index)}.</span>
-                <span className="ml-2">{option.content}</span>
+                <div className="flex items-center gap-2">
+                  <img
+                    className="w-36 max-w-36 max-h-36 object-contain"
+                    src={option.pic}
+                  />
+                  <span className="text-xs">{option.content}</span>
+                </div>
               </div>
             );
           })}
         </div>
-        <div className="w-full border border-t flex items-center fixed left-0 bottom-0 children:rounded-none children:h-14">
-          <Button disabled={currentQuestion===0} block onClick={pre}>
+        <div className="w-full border border-t flex items-center fixed left-0 bottom-0 children:rounded-none children:h-14 bg-white">
+          <Button disabled={questionIndex === 0} block onClick={pre}>
             上一题
           </Button>
           <Button block color="primary" onClick={next}>
